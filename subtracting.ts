f5f4266f680e088ea8812f9fa7ab2be2f25ea3db
nadiia.nikolaienko <nadiia.nikolaienko@gmail.com>
2022-07-30 05:02:46 -0400

    subtraction numbers

--- subtracting.ts
+++ subtracting.ts
@@ -1,4 +1,4 @@
-console.log(50-20);
-console.log('hello world')
-console.log(5)
-console.log(1,2,3,2+3);
+console.log(50 - 20);
+console.log('hello world');
+console.log(5);
+console.log(123, 2 + 3);
